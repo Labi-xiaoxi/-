d7e7f271f900a9aa4a5947f28140e9497bdaf262
Labi-xiaoxi <2790587509@qq.com>
2024-09-23 10:05:13 +0800

    Add files via upload

---
+++ 圆形螺旋.py
@@ -0,0 +1,25 @@
+import turtle as k
+k.speed(0)
+
+
+k.color("red")
+for i in range(25):
+    k.circle(i*5)
+    k.left(4)
+
+k.color("blue")
+for i in range(25):
+    k.circle(i*5)
+    k.left(4)
+
+k.color("green")
+for i in range(25):
+    k.circle(i*5)
+    k.left(4)
+
+k.color("yellow")
+for i in range(25):
+    k.circle(i*5)
+    k.left(4)
+
+k.done()
